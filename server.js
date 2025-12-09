3c8d087aced06b616e3a8647498a263e36048aac
A78009866 <ibispaintxarabic@gmail.com>
2025-12-09 16:02:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,23 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// لخدمة الملفات الثابتة (مثل index.html, CSS, الصور)
-app.use(express.static(path.join(__dirname, 'views)));
+// 1. إعداد EJS كمحرك العرض (View Engine)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// 2. لخدمة الملفات الثابتة (مثل CSS إذا أنشأته مستقبلاً)
+app.use(express.static(path.join(__dirname, 'public')));
 
 // المسار الرئيسي لعرض الصفحة
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views, 'index.html'));
+    // نستخدم res.render بدلاً من res.sendFile
+    res.render('index', { 
+        pageTitle: 'أكاديمية المعالي',
+        // يمكن تمرير بيانات المستويات هنا لاحقاً
+    });
 });
 
 // تشغيل الخادم
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
-
-
